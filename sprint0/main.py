783f331f17bcc3d79e21723ebd0ec6596a7240a3
IagoHermida <ihermidap23@fpcoruna.afundacion.org>
2024-10-09 08:58:45 +0200

    main con comentarios

--- sprint0/main.py
+++ sprint0/main.py
@@ -2,10 +2,13 @@ from heroe import Heroe
 from mazmorra import Mazmorra
 
 def main():
+    #Pedimos al usuario que introduzca el nombre de su heroe y posteriormente creamos el personaje con dicho nombre
     nombre_heroe = input("Introduce el nombre de tu héroe: ")
     heroe = Heroe(nombre_heroe)
+    #iniciamos la clase mazmorra con nuestro heroe e invocamos el metodo jugar
     mazmorra = Mazmorra(heroe)
     mazmorra.jugar()
 
+#Ejecutamos el main
 if __name__ == "__main__":
     main()
